924f36c590f0f1f0a3cf46b3bcf6abc3cccbd511
NiNiyas <54862871+NiNiyas@users.noreply.github.com>
2021-04-24 13:20:04 +0530

    Update 2.0

--- src/Slack/sonarr-import.py
+++ src/Slack/sonarr-import.py
@@ -3,7 +3,6 @@ import logging
 import os
 import re
 import sys
-import time
 
 import requests
 
@@ -269,9 +268,11 @@ def main():
     log.info(json.dumps(message, sort_keys=True, indent=4, separators=(',', ': ')))
 
     # Send notification
-    log.info("Sleeping 20 seconds before sending notifications.")
-    time.sleep(20)
     sender = requests.post(script_config.sonarr_slack_url, headers=slack_headers, json=message)
+    if eventtype == "Test":
+        print("Successfully sent test notification.")
+    else:
+        print("Successfully sent notification to Slack.")
 
 
 # Call main
